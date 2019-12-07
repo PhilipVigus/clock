b2b9b6f4e698546c4b636fc75269e23ba4d068f2
PhilipVigus <philipvigus@yahoo.co.uk>
2019-12-07 18:52:57 +0000

    Add a canvas to the page to draw a clock on
    A canvas was added to the page, along with code to check whether
    canvas is supported by the browser running the page.
    
    The code was altered to add functions to display the time
    digitally and as a drawn clock face. The latter doesnt
    actually do anything yet.

--- js/clock.js
+++ js/clock.js
@@ -7,9 +7,27 @@ function displayCurrentTime() {
   // we need to create a new Date every time this is called so it contains the time now
   let time = new Date();
 
-  // set the html text to the current time
-  document.getElementById("clock").innerHTML = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+  // update the digital time
+  updateDigitalDisplay(time);
+
+  // update the analogue time
+  updateAnalogueDisplay(time);  
 
   // set a timer for the next call to this function
   setTimeout(displayCurrentTime, 500);
+}
+
+function updateDigitalDisplay(timeNow) {
+  document.getElementById("clock-digital").innerHTML = `${timeNow.getHours()}:${timeNow.getMinutes()}:${timeNow.getSeconds()}`;
+}
+
+function updateAnalogueDisplay(timeNow) {
+
+  let canvas = document.getElementById("clock-analogue");
+
+  // check to see if canvas is supported. If not then do something funky
+  if (!canvas.getContext("2d")) {
+    alert("Canvas is not supported on this device - sorry!");
+    return;
+  }
 }
