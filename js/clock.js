e27b628a862568ed2ef571033275796c60174d62
PhilipVigus <philipvigus@yahoo.co.uk>
2019-12-07 18:23:52 +0000

    Add code to continually update the page to the current time

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,15 @@
 'use strict';
 
-let clockEle = document.getElementById("clock");
-const time = new Date();
-clockEle.innerHTML = `Current time = ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+displayCurrentTime();
+
+function displayCurrentTime() {
+
+  // we need to create a new Date every time this is called so it contains the time now
+  let time = new Date();
+
+  // set the html text to the current time
+  document.getElementById("clock").innerHTML = `Current time = ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+
+  // set a timer for the next call to this function
+  setTimeout(displayCurrentTime, 500);
+}
