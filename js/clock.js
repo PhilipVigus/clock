42f58101d66bef46eca058b65452382d2f0c1b68
PhilipVigus <philipvigus@yahoo.co.uk>
2019-12-09 16:51:14 +0000

    Add code to draw clock edge and outer markers
    The clock now shows a circle for its outer edge, and minute
    markers around its edge.

--- js/clock.js
+++ js/clock.js
@@ -18,7 +18,9 @@ function displayCurrentTime() {
 }
 
 function updateDigitalDisplay(timeNow) {
-  document.getElementById("clock-digital").innerHTML = `${formatTimeComponent(timeNow.getHours())}:${formatTimeComponent(timeNow.getMinutes())}:${formatTimeComponent(timeNow.getSeconds())}`;
+  const timeString = `${formatTimeComponent(timeNow.getHours())}:${formatTimeComponent(timeNow.getMinutes())}:${formatTimeComponent(timeNow.getSeconds())}`;
+  const dateString = `${timeNow.getDate()}-${timeNow.getMonth() + 1}-${timeNow.getFullYear()}`;
+  document.getElementById("clock-digital").innerHTML = dateString + "\n" + timeString;
 }
 
 function formatTimeComponent(component) {
@@ -36,7 +38,52 @@ function updateAnalogueDisplay(timeNow) {
     return;
   }
 
-  context.fillStyle = 'rgb(200, 0, 0)';
-  context.fillRect(10, 10, 50, 50);
+  context.lineWidth = 2;
 
+  // draw the clock's outer circle
+  drawCircle([250, 250], 200, context);
+
+  // draw the centre of the clock
+  drawCircle([250, 250], 5, context);
+
+  // draw the outer markers
+  const angleInc = 2 * Math.PI / 60;
+  let startX, startY;
+  let endX, endY;
+
+  for (let i = 0; i < 60; i++) {
+    startX = Math.sin(i * angleInc) * 190;
+    startY = Math.cos(i * angleInc) * 190;
+    endX = Math.sin(i * angleInc) * 200;
+    endY = Math.cos(i * angleInc) * 200;
+
+    drawLine([250 + startX, 250 + startY], [250 + endX, 250 + endY], context);
+  }
+
+  /* // draw the hour hand
+  let hours = 0;
+  if (timeNow.getHours() > 12) {
+    hours = timeNow.getHours() - 12;
+  } else {
+    hours = timeNow.getHours();
+  }
+  hours = 24;
+  endX = Math.sin(hours * angleInc) * 200;
+  endY = Math.cos(hours * angleInc) * 200;
+  drawLine([250, 250], [250 + endX, 250 + endY], context); */
+
+
+}
+
+function drawLine(start, end, context) {
+  context.beginPath();
+  context.moveTo(start[0], start[1]);
+  context.lineTo(end[0], end[1]);
+  context.stroke();
+}
+
+function drawCircle(origin, radius, context) {
+  context.beginPath();
+  context.arc(origin[0], origin[1], radius, 0, 2 * Math.PI);
+  context.stroke();
 }
