c1ce4332f3770184ad27321e7232df2c611069cf
PhilipVigus <philipvigus@yahoo.co.uk>
2019-12-10 09:48:00 +0000

    Add functions to handle saving, retrieving and clearing settings
    Functions were added that handled the saving, retrieval and
    clearing of the clock settings using Local Storage.

--- js/settings.js
+++ js/settings.js
@@ -1,15 +1,19 @@
 'use strict';
+
 if (storageAvailable('localStorage')) {
     // Yippee! We can use localStorage awesomeness
-    alert("OK");
-  }
-  else {
+} else {
     // Too bad, no localStorage for us
     alert("Not OK");
-  }
+}
 
+// populate the page with either stored values or defaults
+retrieveStoredValues();
+
+// function copied from https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
 function storageAvailable(type) {
-    var storage;
+
+    let storage;
     try {
         storage = window[type];
         var x = '__storage_test__';
@@ -31,4 +35,32 @@ function storageAvailable(type) {
             // acknowledge QuotaExceededError only if there's something already stored
             (storage && storage.length !== 0);
     }
+}
+
+function retrieveStoredValues() {
+
+    if (!localStorage.getItem("clock-dig-format") || localStorage.getItem("clock-dig-format") === "12h") {
+        // the setting isn't present, or it was found to be 12h
+        document.getElementsByName("time-format")[0].checked = true;
+    } else {
+        // the setting was found to be 24h
+        document.getElementsByName("time-format")[1].checked = true;
+    }
+
+    localStorage.clear();
+}
+
+function saveValuesToLocalStorage() {
+
+    // store the time format value according to which radio button is checked
+    if (document.getElementsByName("time-format")[0].checked === true) {
+        localStorage.setItem("clock-dig-format", "12h");
+    } else {
+        localStorage.setItem("clock-dig-format", "24h");
+    }
+}
+
+// wipes all of the data storage for this domain
+function clearStoredValues() {
+    localStorage.clear();
 }
